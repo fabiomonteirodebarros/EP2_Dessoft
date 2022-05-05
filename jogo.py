fa315726654d7de534eb4a2f919863794e9f511d
Fabio Monteiro de Barros Neto <fabiombn@al.insper.edu.br>
2022-05-05 17:21:02 -0300

    Terminei de passar as funções e comecei os prints do inicio do jogo

---
+++ jogo.py
@@ -0,0 +1,14 @@
+print (" ============================ ")
+print ("|                            |")
+print ("| Bem-vindo ao Insper Países |")
+print ("|                            |")
+print (" ==== Design de Software ==== ")
+
+
+print ("\nComandos:")
+
+print ("\ndica        - entra no mercado de dicas ")
+print ("desisto     - desiste da rodada ")
+print ("inventario  - exibe sua posição ")
+
+
